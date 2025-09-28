6805ebede59a823f3fe6bd42e655644f680d3f2c
Mahya Tazike <155567461+mtazike@users.noreply.github.com>
2025-09-28 16:09:22 +0000

    Exercise 1 Done!

--- app.py
+++ app.py
@@ -5,21 +5,20 @@ from apputil import *
 # Load Titanic dataset
 df = pd.read_csv('https://raw.githubusercontent.com/leontoddjohnson/datasets/main/data/titanic.csv')
 
-st.write(
-'''
-# Titanic Visualization 1
+# Generate survival demographics table
+df_result = survival_demographics(df)
 
-'''
-)
+st.subheader("Titanic Visualization 1")
+st.write("Question: Were children in third class more likely to survive than adults in third class?")
+                 
 # Generate and display the figure
-fig1 = visualize_demographic()
+fig1 = visualize_demographic(df_result)
 st.plotly_chart(fig1, use_container_width=True)
 
-st.write(
-'''
-# Titanic Visualization 2
-'''
-)
+
+
+st.write(""".......""")
+         
 # Generate and display the figure
 fig2 = visualize_families()
 st.plotly_chart(fig2, use_container_width=True)
--- apputil.py
+++ apputil.py
@@ -1,4 +1,57 @@
 import plotly.express as px
 import pandas as pd
 
-# update/add code below ...
+# Exercise 1
+def survival_demographics(df):
+    # Step 1: create AgeGroup column
+    df['AgeGroup'] = pd.cut(
+        df['Age'],
+        bins=[0, 12, 19, 59, 120],
+        labels=['Child', 'Teen', 'Adult', 'Senior']
+    )
+    
+    # Step 2 + 3: group and calculate
+    grouped = df.groupby(['Pclass', 'Sex', 'AgeGroup'])
+    
+    # Step 4: aggregate
+    result = grouped['Survived'].agg(
+        n_passengers="count",
+        n_survivors="sum",
+        survival_rate="mean"
+    ).reset_index()
+    
+    
+     # Step 5: sort result by survival_rate descending
+    result = result.sort_values(
+        by=['Pclass', 'Sex', 'survival_rate'], 
+        ascending=[True, True, False]
+    )
+  
+    return result
+
+# Step 6 + 7: create visualization function
+def visualize_demographic(df_result):
+    subset = df_result[(df_result['Pclass'] == 3) & (df_result['AgeGroup'].isin(['Child', 'Adult']))]
+    
+    fig = px.bar(
+        subset,
+        x="AgeGroup",
+        y="survival_rate",
+        color="Sex",
+        barmode="group",
+        text="survival_rate",   # adds numbers on bars
+        title="Survival Rate: Children vs Adults in 3rd Class",
+        hover_data={
+            "n_passengers": True,
+            "n_survivors": True,
+            "survival_rate": ':.2f'   # format to 2 decimal places
+        }
+    )
+    
+    fig.update_traces(
+        texttemplate='%{text:.2f}',
+    textposition='outside',
+    hovertemplate="Age Group: %{x}<br>Survival Rate: %{y:.2%}<br>Passengers: %{customdata[0]}<br>Survivors: %{customdata[1]}<extra></extra>"
+    )  # format as %
+    
+    return fig
